ebe64b6d1f613e6d7a225c0c5f04fc041ba5f4c3
Marc Martí Ochaita <marcxeresa@gmail.com>
2026-05-20 11:23:13 +0200

    Update main._motor.c

--- coche/main._motor.c
+++ coche/main._motor.c
@@ -1,3 +1,5 @@
+/*** file main.c ***/
+
 #include <stdio.h>
 #include <string.h>
 #include "freertos/FreeRTOS.h"
@@ -8,12 +10,13 @@
 #include "esp_log.h"
 #include "esp_netif.h"
 #include "mqtt_client.h"
+
 #include "motor.h"
+#include "hall_rpm.h"
 
 static const char *TAG = "MQTT_UPV";
 
-/* -------------------------------------------------- */
-/* WiFi */
+// WiFi
 
 #define WIFI_SSID      "MOVISTAR_B780"
 #define WIFI_PASSWORD  "rvp3M7w4tPyyvErv97p3"
@@ -42,10 +45,15 @@ void wifi_init(void)
     ESP_LOGI("WIFI", "Intentando conectar a la red WIFI...");
 }
 
-/* -------------------------------------------------- */
-/* MQTT */
+// MQTT
 
-static esp_mqtt_client_handle_t client = NULL;
+esp_mqtt_client_handle_t client = NULL;
+
+// Variable global del motor
+static float motor_percent = 0.0f;
+
+// Variable global del RPM
+static int rpm_actual = 0;
 
 static void mqtt_event_handler(void *handler_args,
                                esp_event_base_t base,
@@ -55,29 +63,31 @@ static void mqtt_event_handler(void *handler_args,
     esp_mqtt_event_handle_t event = (esp_mqtt_event_handle_t)event_data;
 
     switch (event->event_id) {
+
         case MQTT_EVENT_CONNECTED:
             ESP_LOGI(TAG, "MQTT CONECTADO ");
-            // Nos suscribimos al topic del potenciómetro del mando
-            esp_mqtt_client_subscribe(event->client, "proyecto/pot/valor", 1);
+
+            // Suscripción al potenciómetro del mando
+            esp_mqtt_client_subscribe(event->client, "proyecto/sem/pot/valor", 1);
             break;
 
         case MQTT_EVENT_DATA:
-            // Comprobar el topic (sin terminación nula, asegurar el tamaño)
-            if (strncmp(event->topic, "proyecto/pot/valor", event->topic_len) == 0) {
+            if (strncmp(event->topic, "proyecto/sem/pot/valor", event->topic_len) == 0) {
+
                 char valor_str[16];
                 size_t len = event->data_len;
                 if (len > 15) len = 15;
                 memcpy(valor_str, event->data, len);
                 valor_str[len] = '\0';
 
-                int valor_adc = atoi(valor_str); // Recibe el ADC crudo (0-4095)
-                float percent = ((float)valor_adc) / 4095.0f;
-                // Limitamos
-                if (percent < 0.0f) percent = 0.0f;
-                if (percent > 1.0f) percent = 1.0f;
+                int valor_adc = atoi(valor_str);
+                motor_percent = ((float)valor_adc) / 4095.0f;
 
-                ESP_LOGI(TAG, "Potenciómetro recibido ADC=%d, porcentaje=%.2f", valor_adc, percent);
-                motor_set_power(percent);
+                if (motor_percent < 0.0f) motor_percent = 0.0f;
+                if (motor_percent > 1.0f) motor_percent = 1.0f;
+
+                ESP_LOGI(TAG, "Potenciómetro recibido ADC=%d, porcentaje=%.2f",
+                         valor_adc, motor_percent);
             }
             break;
 
@@ -102,6 +112,42 @@ void mqtt_init(void)
     esp_mqtt_client_start(client);
 }
 
+// TAREAS DEL MOTOR
+
+// Tarea que recibe el potenciómetro
+// Esta tarea aplica el valor al motor
+void task_motor_tx_pwm(void *pv)
+{
+    while (1) {
+        motor_set_power(motor_percent);
+        vTaskDelay(pdMS_TO_TICKS(50)); // 20 Hz
+    }
+}
+
+/* -------------------------------------------------- */
+//TAREAS DEL RPM
+
+// Tarea que calcula RPM cada 1 segundo
+void task_rpm_count(void *pv)
+{
+    while (1) {
+        rpm_actual = hall_rpm_get_rpm();
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+// Tarea que publica RPM por MQTT
+void task_rpm_publish(void *pv)
+{
+    char msg[16];
+
+    while (1) {
+        sprintf(msg, "%d", rpm_actual);
+        esp_mqtt_client_publish(client, "proyecto/sem/rpm/valor", msg, 0, 1, 0);
+        vTaskDelay(pdMS_TO_TICKS(500));
+    }
+}
+
 /* -------------------------------------------------- */
 
 void app_main(void)
@@ -111,16 +157,24 @@ void app_main(void)
     nvs_flash_init();
     wifi_init();
 
-    /* Damos tiempo REAL al WiFi */
     vTaskDelay(pdMS_TO_TICKS(15000));
 
     mqtt_init();
-
-    // Inicializar el motor
     motor_init();
+    hall_rpm_init();
 
-    // El control es reactivo al topic MQTT, no se necesita bucle ni tareas periódicas
+    // TAREAS DEL MOTOR
+
+    xTaskCreate(task_motor_tx_pwm, "task_motor_tx_pwm", 2048, NULL, 4, NULL);
+
+    // TAREAS DEL RPM
+
+    xTaskCreate(task_rpm_count,   "task_rpm_count",   2048, NULL, 4, NULL);
+    xTaskCreate(task_rpm_publish, "task_rpm_publish", 2048, NULL, 3, NULL);
+
     while (1) {
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
 }
+
+/*** End of file ***/
