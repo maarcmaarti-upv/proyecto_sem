b194acc3193f53d43eee33d5ed80a75df78901a9
Aznar19 <145457870+Aznar19@users.noreply.github.com>
2026-05-26 18:06:31 +0200

    Update lcd_i2c.h

--- mando/lcd_i2c.h
+++ mando/lcd_i2c.h
@@ -1,16 +1,43 @@
+/**
+    Copyright (C) 2025 The Sistemas Empotrados subject at UPV
+    
+    @file    lcd_i2c.h
+    @author  Angel Perles / Modificado por Estudiante
+    @version V0.4
+    @date    2026-05-26
+    @brief   Controlador para la pantalla LCD mediante bus I2C
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef LCD_I2C_H
 #define LCD_I2C_H
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
 #include "driver/i2c.h"
 #include <stdint.h>
 
-#define LCD_I2C_ADDR 0x27
-#define LCD_COLS 16
-#define LCD_ROWS 2
+/* Exported types ------------------------------------------------------------*/
 
+/* Exported constants --------------------------------------------------------*/
+#define LCD_I2C_ADDR 0x27
+#define LCD_COLS     16
+#define LCD_ROWS     2
+
+/* Exported macro ------------------------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/ 
 void lcd_init(void);
 void lcd_clear(void);
 void lcd_set_cursor(uint8_t col, uint8_t row);
-void lcd_print(const char *str);
+void lcd_print(char const * p_str);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* LCD_I2C_H */
+/*** End of file **************************************************************/
