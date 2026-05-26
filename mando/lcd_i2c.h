499ef11eaaa8b6663432b53f07ee20d4ca6a53e6
Aznar19 <145457870+Aznar19@users.noreply.github.com>
2026-05-26 18:14:46 +0200

    Update lcd_i2c.h

--- mando/lcd_i2c.h
+++ mando/lcd_i2c.h
@@ -5,7 +5,7 @@
     @author  Grupo PCNT
     @version V0.4
     @date    2026-05-26
-    @brief   Controlador para la pantalla LCD mediante bus I2C
+    @brief   Template de cabecera aplicado al controlador LCD I2C
 */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
@@ -24,8 +24,8 @@
 
 /* Exported constants --------------------------------------------------------*/
 #define LCD_I2C_ADDR 0x27
-#define LCD_COLS     16
-#define LCD_ROWS     2
+#define LCD_COLS 16
+#define LCD_ROWS 2
 
 /* Exported macro ------------------------------------------------------------*/
 
@@ -33,11 +33,11 @@
 void lcd_init(void);
 void lcd_clear(void);
 void lcd_set_cursor(uint8_t col, uint8_t row);
-void lcd_print(char const * p_str);
+void lcd_print(const char *str);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* LCD_I2C_H */
+#endif
 /*** End of file **************************************************************/
