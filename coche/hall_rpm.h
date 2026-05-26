d9f84fdb4429a4146fa79b3b020eb79e4140b366
Aznar19 <145457870+Aznar19@users.noreply.github.com>
2026-05-26 18:39:24 +0200

    Update hall_rpm.h

--- coche/hall_rpm.h
+++ coche/hall_rpm.h
@@ -1,11 +1,37 @@
-/*** file hall_rpm.h ***/
+/**
+    Copyright (C) 2025 The Sistemas Empotrados subject at UPV
+    
+    @file    hall_rpm.h
+    @author  Grupo PCNT
+    @version V0.4
+    @date    2026-05-26
+    @brief   Template de cabecera para la medición de RPM mediante sensor Hall
+*/
 
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef HALL_RPM_H
 #define HALL_RPM_H
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
 #include "driver/pcnt.h"
 
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Exported macro ------------------------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/ 
 void hall_rpm_init(void);
 int hall_rpm_get_rpm(void);
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif
+/*** End of file **************************************************************/
