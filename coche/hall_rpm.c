06544dc58364ad4c852c6d79db2f251ea251c540
Aznar19 <145457870+Aznar19@users.noreply.github.com>
2026-05-26 18:39:04 +0200

    Update hall_rpm.c

--- coche/hall_rpm.c
+++ coche/hall_rpm.c
@@ -1,5 +1,14 @@
-/*** file hall_rpm.c ***/
+/**
+    Copyright (C) 2025 The Sistemas Empotrados subject at UPV
+    
+    @file    hall_rpm.c
+    @author  Grupo PCNT
+    @version V0.4
+    @date    2026-05-26
+    @brief   Cálculo de velocidad angular y filtro de media móvil para el captador Hall
+*/
 
+/* Includes ------------------------------------------------------------------*/
 #include <stdio.h>
 #include "hall_rpm.h"
 #include "driver/pcnt.h"
@@ -7,18 +16,32 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
+/* Private typedef -----------------------------------------------------------*/
+
+/* Private define ------------------------------------------------------------*/
 #define HALL_GPIO GPIO_NUM_4
 #define PCNT_UNIT PCNT_UNIT_0
 #define PCNT_CHANNEL PCNT_CHANNEL_0
 
-// Media móvil
+/* Media móvil */
 #define N 5
+
+/* Private macro -------------------------------------------------------------*/
+
+/* Private variables ---------------------------------------------------------*/
 static int rpm_buffer[N] = {0};
 static int idx = 0;
 static int sum = 0;
 
 static int last_rpm = 0;
 
+/* Private function prototypes -----------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/
+
+/**
+    @brief  Configura la unidad del contador de pulsos (PCNT) por hardware y su filtro antirrebotes
+*/
 void hall_rpm_init(void)
 {
     pcnt_config_t pcnt_config = {
@@ -36,7 +59,7 @@ void hall_rpm_init(void)
 
     pcnt_unit_config(&pcnt_config);
 
-    // Filtro hardware (5 ms)
+    /* Filtro hardware (5 ms) */
     pcnt_set_filter_value(PCNT_UNIT, 5000);
     pcnt_filter_enable(PCNT_UNIT);
 
@@ -45,20 +68,24 @@ void hall_rpm_init(void)
     pcnt_counter_resume(PCNT_UNIT);
 }
 
+/**
+    @brief  Obtiene los pulsos acumulados, calcula la velocidad instantánea y aplica media móvil
+    @retval Último valor de RPM filtrado
+*/
 int hall_rpm_get_rpm(void)
 {
     int16_t pulses = 0;
 
-    // Leer pulsos
+    /* Leer pulsos acumulados */
     pcnt_get_counter_value(PCNT_UNIT, &pulses);
 
-    // Limpiar para siguiente ventana
+    /* Limpiar para siguiente ventana temporal */
     pcnt_counter_clear(PCNT_UNIT);
 
-    // Calcular RPM
+    /* Calcular RPM brutas */
     int rpm = pulses * 60;
 
-    // Media móvil
+    /* Algoritmo iterativo de media móvil */
     sum -= rpm_buffer[idx];
     rpm_buffer[idx] = rpm;
     sum += rpm_buffer[idx];
@@ -68,3 +95,7 @@ int hall_rpm_get_rpm(void)
 
     return last_rpm;
 }
+
+/* Private functions ---------------------------------------------------------*/
+
+/* End of file ****************************************************************/
