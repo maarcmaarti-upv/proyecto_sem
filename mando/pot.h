7941973742d65e3a7e1a3fe6c253ea69e5b7a75f
Aznar19 <145457870+Aznar19@users.noreply.github.com>
2026-05-26 18:10:51 +0200

    Update pot.h

--- mando/pot.h
+++ mando/pot.h
@@ -1,7 +1,37 @@
+/**
+    Copyright (C) 2025 The Sistemas Empotrados subject at UPV
+    
+    @file    pot.h
+    @author  Grupo PCNT
+    @version V0.4
+    @date    2026-05-26
+    @brief   Librería para la lectura limpia del potenciómetro usando tipos fijos
+*/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef POT_H
 #define POT_H
 
-int pot_read_raw(void);
-void pot_init(void);
-
+#ifdef __cplusplus
+ extern "C" {
 #endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Exported macro ------------------------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/ 
+void pot_init(void);
+int32_t pot_read_raw(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* POT_H */
+/*** End of file ***/
