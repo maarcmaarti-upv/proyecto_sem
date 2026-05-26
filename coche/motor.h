b19440a5d6ddf45aa59a42649a51bd48e5d12dc3
Aznar19 <145457870+Aznar19@users.noreply.github.com>
2026-05-26 18:39:59 +0200

    Update motor.h

--- coche/motor.h
+++ coche/motor.h
@@ -1,11 +1,36 @@
-/*** file motor.h ****/
+/**
+    Copyright (C) 2025 The Sistemas Empotrados subject at UPV
+    
+    @file    motor.h
+    @author  Grupo PCNT
+    @version V0.4
+    @date    2026-05-26
+    @brief   Template de cabecera para la etapa de potencia del motor DC mediante PWM
+*/
 
+/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef MOTOR_H
 #define MOTOR_H
 
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+
+/* Exported types ------------------------------------------------------------*/
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Exported macro ------------------------------------------------------------*/
+
+/* Exported functions --------------------------------------------------------*/ 
 void motor_init(void);
 void motor_set_power(float percent);
 
+#ifdef __cplusplus
+}
 #endif
 
-/*** End of file ****/
+#endif
+/*** End of file **************************************************************/
